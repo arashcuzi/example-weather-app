8775c555a174139490c20e2c241ba5e78ed0f2ee
Shawn Petros <arashcuzi@gmail.com>
2017-05-10 21:38:27 -0500

    cleaned up server and added comments

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var express = require('express');
+const express = require('express');
+const weatherData = require('./data');
+const utils = require('./utils');
 
-var app = express();
+const app = express();
 
 const {
   HOST = 'localhost',
   PORT = 3000
 } = process.env;
 
-app.use(express.static('public'));
+// default route to check app is running
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
+// mock endpoint to get the /weather mock or call the fictional api
+app.get('/weather', (req, res) => {
+  const data = utils.getWeather(true); // call mock
+  res.status(200).json(data); // return data to client
+});
+
+// start server and listen on host:port
 app.listen(PORT, HOST, (err) => {
   if (err) console.log(err);
 
