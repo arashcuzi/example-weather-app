74c4b8a1dee329ac6eb1a3d4c901e19faa0b4600
Shawn Petros <arashcuzi@gmail.com>
2017-05-10 21:39:25 -0500

    added mock data

---
+++ data/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "cities" : [
+    {
+      "name": "Los Angeles, CA",
+      "temp": 85
+    },
+    {
+      "name": "San Diego, CA",
+      "temp": 75
+    },
+    {
+      "name": "San Luis Obispo, CA",
+      "temp": 65
+    },
+    {
+      "name": "San Fransisco, CA",
+      "temp": 55
+    },
+    {
+      "name": "Big Bear, CA",
+      "temp": 45
+    },
+    {
+      "name": "Aspen, CO",
+      "temp": 35
+    }
+  ]
+}
